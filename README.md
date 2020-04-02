37b9cf9037092e747cedff58ee9083d9acd9054e
Christopher <chrissantista@gmail.com>
2020-04-02 10:55:25 -0300

    Renomeado projeto, criado componente login

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CartuchoWebapp
+# CartuchoWebApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiComponentsModule } from './ui-components/ui-components.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
